154502a0ac44739d22ada9035c6ae81236ed87e7
Carolina Luchetta <carolina.luchetta@gmail.com>
2024-05-11 23:53:58 -0300

    Adicionadas instruções de como rodar localmente e no ambiente do alwaysdata.net

--- create_database.sql
+++ create_database.sql
@@ -36,6 +36,8 @@ rm lembremed/migrations/00*
 
 python manage.py makemigrations
 
+> configurar o usuario do banco em lembremed_proj/settings.py
+
 python manage.py migrate
 
 python manage.py populate_db
@@ -49,6 +51,32 @@ python manage.py runserver
 
 
 
+
+## No alwaysdata.net
+> habilitar o login com senha no ssh
+> criar o usuario do banco de dados mysql
+> criar o banco de dados <usuario>_db, dando todas as permissoes ao usuario recem criado
+> entrar no ssh, entrar na pasta /home/<usuario>/www
+git clone https://github.com/samueldata/lembremed
+cd lembremed
+python -m venv env
+source env/bin/activate
+pip install Django
+pip install mysqlclient
+sed -i "s|^ALLOWED_HOSTS = .*|ALLOWED_HOSTS = [\'*']|" lembremed_proj/settings.py
+> configurar o usuario do banco em lembremed_proj/settings.py
+> o host era mysql-<usuario>.alwaysdata.net
+nano lembremed_proj/settings.py
+
+> configuracoes do site python wsgi
+Application path*
+[/home/lembremed/]:www/lembremed/lembremed_proj/wsgi.py
+Working directory
+[/home/lembremed/]:www/lembremed
+Python version
+3.11
+virtualenv directory
+[/home/lembremed/]:www/lembremed/env
 
 
 */
