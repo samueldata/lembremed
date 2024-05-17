6846220f380fdb3b54473e1c4fcfa1b01d2b2ecc
Carolina Luchetta <carolina.luchetta@gmail.com>
2024-05-17 13:00:36 -0300

    Adicionada a capacidade da prescricao ser fracionada, pois o tipo number no input soh deixa ser inteiro; o separacao decimal eh feita com '.' e nao ','   .

--- lembremed/models.py
+++ lembremed/models.py
@@ -27,7 +27,6 @@ class Profissional(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     instituicao = models.ForeignKey(Instituicao, on_delete=models.CASCADE)
     nome = models.CharField(max_length=50)
-
     coren = models.CharField(max_length=10)  # Alterado de IntegerField para CharField
 
     class Meta:
