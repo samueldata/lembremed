4972e142f0372692a508a1f6418656ad80c4c8a8
Carolina Luchetta <carolina.luchetta@gmail.com>
2024-11-06 08:26:57 -0300

    modificacoes com responsavel

--- lembremed/views_instituicao.py
+++ lembremed/views_instituicao.py
@@ -12,71 +12,71 @@ from lembremed.decorators import adiciona_contexto
 @adiciona_contexto
 @permission_required('lembremed.pode_gerenciar_instituicao')
 def instituicao_listar(request, contexto_padrao):
-    instituicoes = Instituicao.objects.all()
-    context = {'lista_instituicoes': instituicoes}
-    
-    return render(request, 'instituicao/index.html', {**context, **contexto_padrao})
+	instituicoes = Instituicao.objects.all()
+	context = {'lista_instituicoes': instituicoes}
+
+	return render(request, 'instituicao/index.html', {**context, **contexto_padrao})
 
 
 @permission_required('lembremed.pode_gerenciar_instituicao')
 def instituicao_editar(request, pcnpj):
-    instituicao = Instituicao.objects.filter(cnpj=pcnpj)[0]
-    context = {
-        'instituicao': instituicao,
-        'usuario': instituicao.usuario,
-        }
-    return render(request, 'instituicao/cadastro.html', context)
+	instituicao = Instituicao.objects.filter(cnpj=pcnpj)[0]
+	context = {
+		'instituicao': instituicao,
+		'usuario': instituicao.usuario,
+		}
+	return render(request, 'instituicao/cadastro.html', context)
 
 
 @permission_required('lembremed.pode_gerenciar_instituicao')
 def instituicao_cadastrar(request):
-    context = {}
-    return render(request, 'instituicao/cadastro.html', context)
+	context = {}
+	return render(request, 'instituicao/cadastro.html', context)
 
 
 @permission_required('lembremed.pode_gerenciar_instituicao')
 def instituicao_salvar(request):
-    if request.method == 'POST':
-        # Pegando a variável POST
-        pcnpj = request.POST.get('cnpj')
-        pnome = request.POST.get('nome')
-        pemail = request.POST.get('email')
-        psenha = request.POST.get('senha')
+	if request.method == 'POST':
+		# Pegando a variável POST
+		pcnpj = request.POST.get('cnpj')
+		pnome = request.POST.get('nome')
+		pemail = request.POST.get('email')
+		psenha = request.POST.get('senha')
 
-        #Verifica se estah editando
-        if (request.POST.get('edit')):
-            instituicao = Instituicao.objects.get(cnpj=pcnpj)
-            instituicao.nome = pnome
-            instituicao.save()
+		#Verifica se estah editando
+		if (request.POST.get('edit')):
+			instituicao = Instituicao.objects.get(cnpj=pcnpj)
+			instituicao.nome = pnome
+			instituicao.save()
 
-            usuario = instituicao.usuario
-            usuario.email = pemail
-            if (psenha):
-                usuario.set_password(psenha)
-            usuario.save()
+			usuario = instituicao.usuario
+			usuario.email = pemail
+			if (psenha):
+				usuario.set_password(psenha)
+			usuario.save()
 
-        else:
-            usuario = get_user_model().objects.create_user(username=pcnpj, email=pemail, password=psenha)
-            usuario.user_permissions.add(Permission.objects.get(codename='pode_gerenciar_morador'))
-            usuario.user_permissions.add(Permission.objects.get(codename='pode_gerenciar_profissional'))
-            usuario.save()
+		else:
+			usuario = get_user_model().objects.create_user(username=pcnpj, email=pemail, password=psenha)
+			usuario.user_permissions.add(Permission.objects.get(codename='pode_gerenciar_morador'))
+			usuario.user_permissions.add(Permission.objects.get(codename='pode_gerenciar_profissional'))
+			usuario.save()
 
-            instituicao = Instituicao.objects.create(cnpj=pcnpj, nome=pnome, usuario=usuario)
-            
-        return HttpResponse("instituicao salvo com sucesso")
-    else:
-        return HttpResponse("erro por GET no salvar")
+			instituicao = Instituicao.objects.create(cnpj=pcnpj, nome=pnome, usuario=usuario)
+
+		return HttpResponse("instituicao salvo com sucesso")
+	else:
+		return HttpResponse("erro por GET no salvar")
 
 
 @permission_required('lembremed.pode_gerenciar_instituicao')
 def instituicao_excluir(request, pcnpj):
-    #Verifica se o cnpj existe
-    instituicao = Instituicao.objects.get(cnpj=pcnpj)
-    if (instituicao):
-        instituicao.delete()
+	#Verifica se o cnpj existe
+	instituicao = Instituicao.objects.get(cnpj=pcnpj)
+	if (instituicao):
+		instituicao.delete()
 
-        instituicao.usuario.delete()
-        
-        return HttpResponse("excluido com sucesso")
-    else:
-        return HttpResponse("Erro ao localizar cnpj")
+		instituicao.usuario.delete()
+
+		return HttpResponse("excluido com sucesso")
+	else:
+		return HttpResponse("Erro ao localizar cnpj")
--- lembremed/views_morador.py
+++ lembremed/views_morador.py
@@ -1,81 +1,88 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Morador, Instituicao, Profissional
+from .models import Morador, Responsavel, Instituicao, Profissional
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.contrib.auth.decorators import permission_required
 from lembremed.decorators import adiciona_contexto
+import requests
 
 #Pagina principal dos moradores
 #Lista todos os moradores
 @adiciona_contexto
 @permission_required('lembremed.pode_gerenciar_morador')
 def morador_listar(request, contexto_padrao):
-    #Verifica se eh profissional ou instituicao cadastrando
-    print("\n", (contexto_padrao['usuario']), "\n\n")
-    if (isinstance(contexto_padrao['usuario'], Instituicao)):
-        moradores = Morador.objects.filter(instituicao=contexto_padrao['usuario'])
+	#Verifica se eh profissional ou instituicao cadastrando
+	print("\n", (contexto_padrao['usuario']), "\n\n")
+	if (isinstance(contexto_padrao['usuario'], Instituicao)):
+		moradores = Morador.objects.filter(instituicao=contexto_padrao['usuario'])
 
-    elif(isinstance(contexto_padrao['usuario'], Profissional)):
-        moradores = Morador.objects.filter(instituicao=contexto_padrao['usuario'].instituicao)
-    
-    context = {'lista_moradores': moradores}
-    return render(request, 'morador/index.html', {**context, **contexto_padrao})
+	elif(isinstance(contexto_padrao['usuario'], Profissional)):
+		moradores = Morador.objects.filter(instituicao=contexto_padrao['usuario'].instituicao)
+
+	context = {'lista_moradores': moradores}
+	return render(request, 'morador/index.html', {**context, **contexto_padrao})
 
 
 @permission_required('lembremed.pode_gerenciar_morador')
 def morador_editar(request, pcpf):
-    morador = Morador.objects.filter(cpf=pcpf)[0]
-    #return HttpResponse(morador.nome)
-    context = {'morador': morador}
-    return render(request, 'morador/cadastro.html', context)
+	morador = Morador.objects.filter(cpf=pcpf)[0]
+	#return HttpResponse(morador.nome)
+	context = {'morador': morador}
+	return render(request, 'morador/cadastro.html', context)
 
 
 @permission_required('lembremed.pode_gerenciar_morador')
 def morador_cadastrar(request):
-    context = {}
-    return render(request, 'morador/cadastro.html', context)
+	context = {}
+	return render(request, 'morador/cadastro.html', context)
 
 
 @adiciona_contexto
 @permission_required('lembremed.pode_gerenciar_morador')
 def morador_salvar(request, contexto_padrao):
-    if request.method == 'POST':
-        # Pegando a variável POST
-        pcpf = request.POST.get('cpf')
-        pnome = request.POST.get('nome')
-        pdt_nascimento = request.POST.get('dt_nascimento')
+	if request.method == 'POST':
+		# Pegando a variável POST
+		pcpf = request.POST.get('cpf')
+		pnome = request.POST.get('nome')
+		pdt_nascimento = request.POST.get('dt_nascimento')
 
-        #Verifica se estah editando
-        if (request.POST.get('edit')):
-            morador = Morador.objects.get(cpf=pcpf)
-            morador.nome = pnome
-            morador.dt_nascimento = pdt_nascimento
-            
+		rcpf = request.POST.get('rcpf')
+		rnome = request.POST.get('rnome')
+		remail = request.POST.get('remail')
 
-        else:
-            morador = Morador(cpf=pcpf, nome=pnome, dt_nascimento=pdt_nascimento)
+		#Verifica se estah editando
+		if (request.POST.get('edit')):
+			morador = Morador.objects.get(cpf=pcpf)
+			morador.nome = pnome
+			morador.dt_nascimento = pdt_nascimento
 
-        #Verifica se eh profissional ou instituicao cadastrando
-        if (isinstance(contexto_padrao['usuario'], Instituicao)):
-            morador.instituicao = contexto_padrao['usuario']
+		else:
+			morador = Morador(cpf=pcpf, nome=pnome, dt_nascimento=pdt_nascimento)
 
-        elif(isinstance(contexto_padrao['usuario'], Profissional)):
-            morador.instituicao = contexto_padrao['usuario'].instituicao
-        
-        morador.save()
+			responsavel = Responsavel(cpf=rcpf, nome=rnome)
+			responsavel.hashcode = requests.get("https://www.uuidgenerator.net/api/version4", headers={"Accept": "application/json"})
 
-        return HttpResponse("morador salvo com sucesso")
-    else:
-        return HttpResponse("erro por GET no salvar")
+		#Verifica se eh profissional ou instituicao cadastrando
+		if (isinstance(contexto_padrao['usuario'], Instituicao)):
+			morador.instituicao = contexto_padrao['usuario']
+
+		elif(isinstance(contexto_padrao['usuario'], Profissional)):
+			morador.instituicao = contexto_padrao['usuario'].instituicao
+
+		morador.save()
+
+		return HttpResponse("Morador salvo com sucesso")
+	else:
+		return HttpResponse("erro por GET no salvar")
 
 
 @permission_required('lembremed.pode_gerenciar_morador')
 def morador_excluir(request, pcpf):
-    #Verifica se o cpf existe
-    morador = Morador.objects.get(cpf=pcpf)
-    if (morador):
-        morador.delete()
-        return HttpResponse("excluido com sucesso")
-    else:
-        return HttpResponse("Erro ao localizar cpf")
+	#Verifica se o cpf existe
+	morador = Morador.objects.get(cpf=pcpf)
+	if (morador):
+		morador.delete()
+		return HttpResponse("excluido com sucesso")
+	else:
+		return HttpResponse("Erro ao localizar cpf")
